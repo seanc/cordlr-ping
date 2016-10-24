06687644e3f95ce547d309942010172f8a517e8a
Sean Wilson <hello@imsean.me>
2016-10-23 20:57:34 -0500

    Fixed defaults

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const pixie = require('pixie');
 
 function ping(bot, config) {
-  const format = config.ping.format || 'Ping {{ping}}';
+  config = config[ping.name] || {};
+  const format = config.format || 'Ping {{ping}}';
   return function run(message, args) {
     const now = Date.now();
     const res = pixie.render(format, {
